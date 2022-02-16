eebba4952f818219889cd10c888c3c26adb72e10
NondasNF <nondasnoronha@gmail.com>
2022-02-16 15:43:13 -0300

    Add suggested fixes

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_202_175_213) do
-  create_table 'contacts', force: :cascade do |t|
-    t.string 'name'
-    t.date 'birthday'
-    t.integer 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_contacts_on_user_id'
+ActiveRecord::Schema.define(version: 2022_02_02_175213) do
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "birthday"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table 'phones', force: :cascade do |t|

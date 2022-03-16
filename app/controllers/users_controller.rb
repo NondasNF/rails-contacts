ad9b0dce63e994483c94965a6c5fc7c377c482f1
NondasNF <nondasnoronha@gmail.com>
2022-03-16 07:04:35 -0300

    Update user model view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,50 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @contacts = Contact.where(user_id: @user.id)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to "/users/#{@user.id}/"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to "/users/#{@user.id}/"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age, :bio)
+  end
+
 end
